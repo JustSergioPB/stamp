f1d0e5413617308635b8008a9569dd355fa204fe
JustSergioPB <justsergioit.com>
2024-05-08 14:11:09 +0200

    Main: Configured linting + global ts-node

---
+++ packages/domain/.eslintrc.js
@@ -0,0 +1,9 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+};
